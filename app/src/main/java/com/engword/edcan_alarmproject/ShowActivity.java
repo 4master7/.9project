b16ce666e78a92cae7fabeae22876b9c839171c8
jtk040812@gmail.com <jtk040812@gmail.com>
2021-08-21 19:45:18 +0900

    last

--- app/src/main/java/com/engword/edcan_alarmproject/ShowActivity.java
+++ app/src/main/java/com/engword/edcan_alarmproject/ShowActivity.java
@@ -2,15 +2,24 @@ package com.engword.edcan_alarmproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class ShowActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class ShowActivity extends AppCompatActivity {
+    private AlarmManager alarmMgr;
+    private PendingIntent pendingIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +39,38 @@ public class ShowActivity extends AppCompatActivity {
             String contText = cont.getText().toString();
             ((ListActivity)ListActivity.mCon).adapter.setmItem(alarm,contText,pos);
             ((ListActivity)ListActivity.mCon).adapter.notifyDataSetChanged();
+            createNotificationChannel();
+            alarmBroadcastReceiver(picker.getHour(),picker.getMinute());
             finish();
         });
         back.setOnClickListener(v->{
             finish();
         });
+    }
+    public void alarmBroadcastReceiver(int h,int m) {
+        Intent alarmBroadcastReceiverintent = new Intent(this, AlarmBroadcastReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, alarmBroadcastReceiverintent, 0);
+
+        alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, h);
+        calendar.set(Calendar.MINUTE, m);
+
+        alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 86400000, pendingIntent);
+    }
+
+    public void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "알림";
+            String description = "Oreo Version 이상을 위한 알림";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel notificationChannel = new NotificationChannel("channel_id", name, importance);
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
